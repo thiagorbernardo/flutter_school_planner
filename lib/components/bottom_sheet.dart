d017ca156f901e062a11e9e1bb9277e663f0b127
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-06-06 22:12:25 -0300

    feat: adapt to show out of list view

--- lib/components/bottom_sheet.dart
+++ lib/components/bottom_sheet.dart
@@ -5,22 +5,31 @@ import 'package:school_planner/theme/app_theme.dart';
 
 class CustomBottomSheet extends GetView<SchoolController> {
   final List<Widget> items;
+  final Widget? customWidget;
   final double height;
   final Color? backgroundColor;
 
   CustomBottomSheet(
-      {required this.items, required this.height, this.backgroundColor});
+      {required this.items,
+      required this.height,
+      this.backgroundColor,
+      this.customWidget});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: this.height,
       color: this.backgroundColor != null ? this.backgroundColor : Colors.white,
-      child: ListView.builder(
-        padding: EdgeInsets.fromLTRB(25, 20, 25, 0),
-        itemCount: this.items.length,
-        itemBuilder: (context, index) => this.items[index],
-      ),
+      child: this.customWidget != null
+          ? Padding(
+              padding: EdgeInsets.fromLTRB(25, 20, 25, 0),
+              child: this.customWidget,
+            )
+          : ListView.builder(
+              padding: EdgeInsets.fromLTRB(25, 20, 25, 0),
+              itemCount: this.items.length,
+              itemBuilder: (context, index) => this.items[index],
+            ),
     );
   }
 }
